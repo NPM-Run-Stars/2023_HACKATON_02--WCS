c2c664318989272df45fb4507aa4a9f6150a6612
Lucas DEGRET <lucas.degret@outlook.fr>
2023-06-28 21:56:56 +0200

    Modification de la findAll en lui passant le query brand

--- backend/src/models/MobileModelsManager.js
+++ backend/src/models/MobileModelsManager.js
@@ -4,6 +4,13 @@ class MobileModelsManager extends AbstractManager {
   constructor() {
     super({ table: "mobile_models" });
   }
+  findAllByBrand(brand) {
+    let url = `select m.id, m.model from ${this.table} m `;
+    if (brand) {
+      url += " INNER JOIN brands b ON m.brand_id = b.id WHERE brand = ?"
+    }
+    return this.database.query(url, [brand]);
+  }
 }
 
 module.exports = MobileModelsManager;
