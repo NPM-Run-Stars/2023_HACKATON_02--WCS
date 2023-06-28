b82901e94aaadc4423309e5ccbd6e75c4c17048f
Lucas DEGRET <lucas.degret@outlook.fr>
2023-06-28 19:20:14 +0200

    Création de la fonction read qui recupère la data en fonction de la marque de tel

--- backend/src/models/BrandsManager.js
+++ backend/src/models/BrandsManager.js
@@ -4,6 +4,12 @@ class BrandsManager extends AbstractManager {
   constructor() {
     super({ table: "brands" });
   }
+
+  findByBrand(brand) {
+    return this.database.query(`select * from  ${this.table} where brand = ?`, [
+      brand,
+    ]);
+  }
 }
 
 module.exports = BrandsManager;
