5786aa7978af96e46c7fd8935d5fce655bdd93fd
Lucas DEGRET <lucas.degret@outlook.fr>
2023-06-28 19:08:55 +0200

    Importation du brandsController + création de la route get all

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,12 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 
-const itemControllers = require("./controllers/itemControllers");
+// const itemControllers = require("./controllers/itemControllers");
 
-router.get("/items", itemControllers.browse);
-router.get("/items/:id", itemControllers.read);
-router.put("/items/:id", itemControllers.edit);
-router.post("/items", itemControllers.add);
-router.delete("/items/:id", itemControllers.destroy);
+// router.get("/items", itemControllers.browse);
+// router.get("/items/:id", itemControllers.read);
+// router.put("/items/:id", itemControllers.edit);
+// router.post("/items", itemControllers.add);
+// router.delete("/items/:id", itemControllers.destroy);
+
+const brandsControllers = require("./controllers/brandsControllers");
+
+router.get("/brands", brandsControllers.browse);
 
 module.exports = router;
